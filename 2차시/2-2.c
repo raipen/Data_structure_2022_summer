77de2b32d2db6d91dbe73239ef839309e5d6a4e1
Widadada <106460325+Widadada@users.noreply.github.com>
2022-07-20 15:41:21 +0900

    Create 2-2.c

---
+++ 2차시/2-2.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main()
+{
+    int row, col;
+    int i, j;
+    int **arr;
+    scanf("%d %d", &row, &col);
+    arr = (int**)malloc(sizeof(int*) * row);
+    for(i=0;i<row;i++)
+        arr[i] = (int*)malloc(sizeof(int) * col);
+    for (i = 0;i < row;i++) {
+        for (j = 0;j < col;j++) {
+            arr[i][j] = pow(i, 2) + pow(j, 2);
+            printf("%4d", arr[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0;i < row;i++)
+        free(arr[i]);
+    free(arr);
+}
