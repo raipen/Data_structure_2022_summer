6082f94538a2f0d6a32bec00b704c42b6ae43005
Jiwon <raipen@knu.ac.kr>
2022-07-22 11:05:34 +0900

    2차시 과제 2번 3번 피드백

--- datastructure_2.2.c
+++ datastructure_2.2.c
@@ -7,23 +7,59 @@ int countZero(int);
 int main(void)
 {
     int num;
+    /*여기서부터 while(1)로 return 0; 앞부분까지 감싸주고,
+    num<0일때 Exit 출력과 함께 break; 해주면
+    음수가 들어올때까지 무한 반복 가능*/
     scanf("%d", &num);
     printf("input num: %d\n", num);
 
     if (num < 0)
         printf("Exit\n");
 
-    int binary = binaryCreate(num);
+    int binary = binaryCreate(num); //이 함수가 최종적으로 이진수로 표현된 값을 리턴해준다는 점에서 진짜 작명도 잘 했고 함수 자체도 잘 돌아감 
     printf("2진 표현: %d\n", binary);
     
+    /*다만 2진수를 출력하는 위 두줄에 한가지 문제가 있는데...
+    binaryCreate(1023) == 1111111111(1이 10개)이고
+    binaryCreate(1024) == 10000000000(1뒤에 0이 10개)인데
+    int의 최대 표현 가능 수는 2,147,483,647 이라서 1024의 답이 제대로 저장될 수 없어 ㅠㅠ
+    그래서 1024 이상의 수를 이진수로 출력하기 위해서는 binaryCreate()가 int를 호출해주는 방식을 쓰기 힘들어
+
+    그래서 그냥 끝에서부터 한번에 한가지 수를 출력하는 방식으로 코드를 짜는 편이야.
+    ex)
+    int main(){
+        int input;
+        while(1){
+            scanf("%d",&input);
+            if(input<0){
+                fprintf(stderr,"Error: invalid input number %d\n",input);
+                break;
+            }
+            printf("2진 표현: ");
+            printBinary(input);
+            printf("\n");
+        }
+        return 0;
+    }
+
+    void printBinary(int num){
+        if(num){
+            printBinary(num / 2);
+            printf("%d\n",num % 2);
+        }
+    }
+
+
+    */
+
     int cnt;
-    cnt = countZero(num);
+    cnt = countZero(num); //얘는 그냥 그저 완벽 그 자체
     printf("%d내 0은 %d개 있다.\n", num, cnt);
 
     return 0;
 }
 
-int binaryCreate(int num){
+int binaryCreate(int num){  
     if (num == 0)
         return 0;
     int result = (num % 2) + binaryCreate(num/2)*10;
@@ -37,6 +73,10 @@ int countZero(int num){
     if (num == 1){
         return 0;
     }
+    // 이거 스위치문 안 쓰고
+    // return countZero(num/2) + ((num+1) % 2);
+    // 라고 한줄만 적었어도 됨
+    // 이건 코딩적인건 아니고.. 그냥 수학적으로 활용하는 팁.
     switch(num % 2){
     case 0:
         return (countZero(num/2) + 1);
--- datastructure_2.3.c
+++ datastructure_2.3.c
@@ -32,6 +32,44 @@ int main(void){
     }
     fclose(f2);
 
+    /*
+    6~18번째줄과 21~33번째 줄이 완벽하게 똑같은 구조를 가지고 있다.
+    이런 경우에는 함수로 처리하는 것이 좋다.
+    ex)
+    int main(void){
+        int* arr1 = readFromFile("in1.txt");
+        int* arr2 = readFromFile("in2.txt");
+
+        for(int i = 0; i < size1; i++)
+            for(int j = 0; j < size2; j++)
+                if(*(arr1+i) == *(arr2 + j))
+                    printf("  %d", *(arr1+i));
+                    
+        free(arr1);
+        free(arr2);
+
+        return 0;
+    }
+    
+    int* readFromFile(char* filename){
+        FILE* fp;
+        if ((fp = fopen(filename, "r")) == NULL){
+            printf("파일이 열리지 않습니다.\n");
+            exit(1);
+        }
+        int size;
+        fscanf(fp, "%d", &size);
+        int *arr = (int*) malloc(sizeof(int)*size);
+
+        for (int i = 0; i < size; i++){
+            fscanf(fp, "%d", arr + i);
+        }
+        fclose(fp);
+        return arr;
+        //여기에서 할당한 arr은 나중에 메인에서 다 쓰고 난 다음에 free 해주니까 여기에서 free 하면 안 된다!!
+    }
+    */
+
     for(int i = 0; i < size1; i++){
         for(int j = 0; j < size2; j++){
             if(*(arr1+i) == *(arr2 + j)){
