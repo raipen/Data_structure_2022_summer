eb6ec6d16dd76b8ef6d0c1638c1b42a280de1592
Widadada <106460325+Widadada@users.noreply.github.com>
2022-07-30 22:43:46 +0900

    Create 4.c

---
+++ 5차시/4.c
@@ -0,0 +1,104 @@
+// 작동되지 않음
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+}NODE;
+
+NODE* insert(NODE* h, int d) {
+    NODE* node, * p, * q;
+    node = (NODE*)malloc(sizeof(NODE));
+    node->data = d;
+
+    p = h;
+    q = h;
+    while (p != NULL) {
+        if (p->next > d)
+            break;
+        q = p;
+        p = p->next;
+    }
+
+    if (p == h) {
+        node->next = h;
+        h = node;
+    }
+    else {
+        node->next = p;
+        q->next = node;
+    }
+    return h;
+}
+
+NODE* delete(NODE* h, int d) {
+    NODE* node, * p, * q;
+    node = (NODE*)malloc(sizeof(NODE));
+    node->data = d;
+
+    p = h;
+    q = h;
+    while (p != NULL) {
+        if (p->next > d)
+            break;
+        q = p;
+        p = p->next;
+    }
+    if (p->data == d) {
+        q->next = p->next;
+        free(p);
+    }
+    else
+        printf("삭제할 수 %d 없음", d);
+    return h;
+}
+
+
+void printNode(NODE* h) {
+    NODE* p;
+    p = h;
+    while (p != NULL) {
+        printf("%d    ", p->data);
+        p = p->next;
+    }
+    p = h;
+  
+    while (p != NULL) {
+        h = h->next;
+        free(p);
+        p = h;
+    }
+    
+    return 0;
+}
+
+int main(void)
+{
+    int a, b;
+    NODE* h=NULL;
+    h = (NODE*)malloc(sizeof(NODE));
+
+    FILE* fp;
+    if ((fp = fopen("list.txt", "r")) == NULL) {
+        printf("Error");
+        return 0;
+    }
+
+    while (fscanf(fp, "%d", &a) != EOF) {
+        h = insert(h, a);
+    }
+
+    fclose(fp);
+
+    while (scanf("%d", &b) >= 0) {
+        if (b % 10 >= 0 && b % 10 <= 4)
+            h = insert(h, b);
+        else
+            h = delete(h, b);
+    }
+
+
+    printNode(h);
+}
