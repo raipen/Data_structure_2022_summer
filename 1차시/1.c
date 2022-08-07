ccb2b2adc63edd92c721a226cfaa4bd383e25ce1
Widadada <106460325+Widadada@users.noreply.github.com>
2022-08-07 17:31:26 +0900

    Update 1.c

--- 1차시/1.c
+++ 1차시/1.c
@@ -1,4 +1,5 @@
-#define CRT_SECURE_NO_WARNINGS
+// 작동O 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,24 +13,29 @@ typedef struct student {
 
 void main()
 {
-    int i=0;
+    int i = 0;
     stu data[5];
     FILE* fp;
-    FILE* fp1;
+    FILE* fp1 = NULL;
+   
     fp = fopen("score.txt", "r");
     fp1 = fopen("result.txt", "w");
+    
     if (fp == NULL) {
-        printf("ERROR");
-        return 0;
+       printf("ERROR");
+       return 0;
     }
+   
 
+   
     while (fscanf(fp, "%s %d %d %d",
         data[i].name, &data[i].s1, &data[i].s2, &data[i].s3) != EOF) {
         data[i].ave = (data[i].s1 + data[i].s2 + data[i].s3) / 3;
-        fprintf(fp, "%s %d\n", data[i].name, data[i].ave);
+        fprintf(fp1, "%s %d\n", data[i].name, data[i].ave);
         printf("%s %d\n", data[i].name, data[i].ave);
         i++;
-    }   
+    }
+   
     fclose(fp);
-    fclose(fp1); 
+    fclose(fp1);
 }
