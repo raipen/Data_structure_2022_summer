976a26d80431867de18689b8c8584de0b9df456e
Widadada <106460325+Widadada@users.noreply.github.com>
2022-08-07 17:34:44 +0900

    Update 2.c

--- 1차시/2.c
+++ 1차시/2.c
@@ -1,3 +1,4 @@
+// 작동O
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,17 +13,17 @@ void main()
     printf("Input size: %d\n", input);
     a1 = (int*)malloc(sizeof(int) * input);
     a2 = (int*)malloc(sizeof(int) * input);
-    printf("Array1:   ");
+    printf("Array1:  ");
     for (i = 0;i < input;i++) {
         a1[i] = rand() % 21;
         printf("%3d", a1[i]);
     }
-    printf("\nArray2:   ");
+    printf("\nArray2:  ");
     for (i = 0;i < input;i++) {
         a2[i] = rand() % 21;
         printf("%3d", a2[i]);
     }
-    printf("\nresult:   ");
+    printf("\nresult:  ");
     for (i = 0;i < input;i++)
         printf("%3d", a1[i] + a2[i]);
     free(a1);
